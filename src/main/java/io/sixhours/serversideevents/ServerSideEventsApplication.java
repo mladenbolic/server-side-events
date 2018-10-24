4bd0ef6e1b58ab2c6ee329e92c5c05e2aeb9a591
Mladen Bolic <mladen.bolic@sixhours.io>
2018-10-24 21:43:54 +0200

    Add springboot project

---
+++ src/main/java/io/sixhours/serversideevents/ServerSideEventsApplication.java
@@ -0,0 +1,12 @@
+package io.sixhours.serversideevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideEventsApplication.class, args);
+	}
+}
